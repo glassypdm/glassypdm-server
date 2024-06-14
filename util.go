efe286639e2819a04992f7cc66fbb16bdffb2ff3
Joshua Tenorio <joshliam09@gmail.com>
2024-06-13 23:58:46 -0700

    undoing desperate fix

--- util.go
+++ util.go
@@ -58,19 +58,15 @@ func createDB() *sql.DB {
 var ddl string
 
 func UseQueries() *sqlcgen.Queries {
-	//ctx := context.Background()
+	ctx := context.Background()
 	db := createDB()
-
-	// TODO figure out the below issue it randomly popped up 240613
-	/*
-		_, err := db.ExecContext(ctx, ddl)
-		if err != nil {
-			//fmt.Println(ddl)
-			fmt.Printf("err: %v\n", err)
-			fmt.Println("oof db")
-			os.Exit(1)
-		}
-	*/
+	_, err := db.ExecContext(ctx, ddl)
+	if err != nil {
+		//fmt.Println(ddl)
+		fmt.Printf("err: %v\n", err)
+		fmt.Println("oof db")
+		os.Exit(1)
+	}
 
 	queries := sqlcgen.New(db)
 	return queries
