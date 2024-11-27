daaf1894fe86a9fd24e51ee8ad0bfe03707bee31
Joshua Tenorio <joshliam09@gmail.com>
2024-11-26 21:20:09 -0700

    TODOs for jwt generation

--- internal/project/project.go
+++ internal/project/project.go
@@ -9,8 +9,20 @@ import (
 var TokenAuth *jwtauth.JWTAuth
 
 func RouteStoreJWTRequest(w http.ResponseWriter, r *http.Request) {
-	RequestStoreJWT("asdf")
+	RequestStoreJWT("asdf", 19, false)
 }
-func RequestStoreJWT(UserID string) {
+func RequestStoreJWT(UserID string, ProjectID int, IsDownload bool) {
+	// TODO this should be in an init function
+
+	// check if user can upload/download to/from ProjectID
+
+	// create JWT if so, otherwise return false
+	// in JWT, include userID, projectID, and intended action
+}
+
+func InitStoreJWT() {
+	// grab secret from dotenv
+
+	// initialize tokenauth
 	TokenAuth = jwtauth.New("HS256", []byte("secret"), nil)
 }
